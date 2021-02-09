b0e21b79e850625f64c97f2b4638e3796736a157
krishnafrabon <krishna.php@frabon.com>
2021-02-09 16:26:37 +0530

    new

---
+++ global_objects.js
@@ -0,0 +1,9 @@
+
+var main = require('./main');
+
+console.log(__dirname);
+
+
+console.log(__filename);
+
+console.log(main.abc);
---
+++ main.js
@@ -0,0 +1,30 @@
+console.log('Hello')
+
+const a = 10
+const b= 20
+c = a+b
+
+console.log(c)
+
+
+var users = {name:"krishna",age:"32"}
+console.log(users.name);
+
+
+var person = function(a,b)
+{
+	 c = a+b
+	console.log(c);
+}
+person(20,20);
+
+
+var users1=(a,b)=> {c = a+b; console.log(c);
+}
+
+users1(70,20);
+
+
+var name ='Krishna';
+
+module.exports.abc= name;
